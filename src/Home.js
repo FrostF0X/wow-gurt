18523081d023b9eea949c54f98adf758e9b2d99b
dims <dmytro.konowalow@gmail.com>
2023-06-12 18:16:41 +0200

    add typing

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,38 @@
 import './styles/Home.css';
+import React from "react";
+import Typed from "typed.js";
 
-function Home() {
-    return (
-        <div className={"home"}>
-            <div className="home-link">
-                <a href="/survey">Survey</a>
+class Home extends React.Component {
+    loaded = false;
+
+    componentDidMount() {
+        if(this.loaded) {
+            return;
+        }
+        new Typed('.home-link>a', {
+            strings: [
+                'Survey&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;',
+                'Business Agility&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;',
+            ],
+            typeSpeed: 50,
+            cursorChar: '█'
+        });
+        this.loaded = true;
+    }
+
+    render() {
+        return (
+            <div className={"home"}>
+                <div className="home-link">
+                    <a href="/survey"></a>
+                </div>
+                <div className={"home-image-container"}>
+                    <img className={"home-image"} src="/ccchaos.svg" alt="ccchaos"/>
+                </div>
             </div>
-            <div className={"home-image-container"}>
-                <img className={"home-image"} src="/ccchaos.svg" alt="ccchaos"/>
-            </div>
-        </div>
-    );
+        );
+    }
+
 }
 
 export default Home;
