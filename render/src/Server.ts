a09fe9ec434ad088638e8d6b7aa22684e81cd3ce
dims <dmytro.konowalow@gmail.com>
2023-07-14 05:48:44 +0200

    fix origin

--- render/src/Server.ts
+++ render/src/Server.ts
@@ -24,17 +24,9 @@ export default class Server {
         }));
         app.use(express.json()); // for parsing application/json
         app.use(cors({
-            origin: (origin, callback) => {
-                var allowedOrigins = ['http://localhost:3000',
-                    'https://wow-gurt.pages.dev'];
-                if (!origin) return callback(null, true);
-                if (allowedOrigins.indexOf(origin) === -1) {
-                    var msg = 'The CORS policy for this site does not ' +
-                        'allow access from the specified Origin.';
-                    return callback(new Error(msg), false);
-                }
-                return callback(null, true);
-            }
+            credentials: true,
+            exposedHeaders: ['Content-Type', 'Depth', 'User-Agent', 'X-File-Size', 'X-Requested-With', 'If-Modified-Since', 'X-File-Name', 'Cache-Control'],
+            origin: '*'
         }));
 
         if (type === "get") {
