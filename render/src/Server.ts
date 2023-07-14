25b1f220f18c64893c499a9ea70df30cc361f195
dims <dmytro.konowalow@gmail.com>
2023-07-14 05:39:25 +0200

    fix origin

--- render/src/Server.ts
+++ render/src/Server.ts
@@ -24,7 +24,17 @@ export default class Server {
         }));
         app.use(express.json()); // for parsing application/json
         app.use(cors({
-            origin: (origin, callback) => callback(null,true)
+            origin: (origin, callback) => {
+                var allowedOrigins = ['http://localhost:3000',
+                    'https://wow-gurt.pages.dev'];
+                if (!origin) return callback(null, true);
+                if (allowedOrigins.indexOf(origin) === -1) {
+                    var msg = 'The CORS policy for this site does not ' +
+                        'allow access from the specified Origin.';
+                    return callback(new Error(msg), false);
+                }
+                return callback(null, true);
+            }
         }));
 
         if (type === "get") {
