ab89025f9465058bbf81cb0748aa84d560dc929a
dims <dmytro.konowalow@gmail.com>
2023-08-07 21:01:48 +0200

    gif reorder pink pixel

--- render/reorder.sh
+++ render/reorder.sh
@@ -11,10 +11,22 @@ ffmpeg -i "$INPUT_GIF" -vsync 0 "$TMP_DIR/frame%d.png"
 
 # Find the frame with the pink pixel at 1x1 position
 START_FRAME=-1
+
+# Sort by the number after "frame" to get them in numerical order
+find "$TMP_DIR" -name 'frame*.png' | sort -t e -k 2 -n | while read -r FILE; do
+  BASENAME=$(basename "$FILE")
+  # Extract the frame number and pad with zeros to get 2 digits
+  NUM=$(printf "%02d" $(echo "$BASENAME" | grep -o '[0-9]\+'))
+  NEWNAME="frame$NUM.png"
+  if [[ "$BASENAME" != "$NEWNAME" ]]; then
+    mv "$FILE" "$TMP_DIR/$NEWNAME"
+  fi
+done
+
 FILES=($TMP_DIR/*.png)
 for ((i = 0; i < ${#FILES[@]}; i++)); do
-  PIXEL=$(convert ${FILES[$i]} -crop 1x1+1+1 -format "%[pixel:u.p{0,0}]" info:)
-  echo "$PIXEL";
+  PIXEL=$(magick "${FILES[$i]}" -format "%[hex:u.p{1,1}]\n" info:)
+  echo "${FILES[$i]}" "$PIXEL"
   if [[ $PIXEL == "#EA42A7" ]]; then
     START_FRAME=$i
     break
