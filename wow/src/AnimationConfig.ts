1537e4329a7862b194ab9a0da9814fadb27f7bb1
dims <dmytro.konowalow@gmail.com>
2023-11-02 21:03:06 +0100

    revert wow 16x screen

--- wow/src/AnimationConfig.ts
+++ wow/src/AnimationConfig.ts
@@ -4,15 +4,15 @@ import Delay from "./Animation/Delay";
 import Color from "./Animation/Color";
 
 export default class AnimationConfig {
-    static levelConfig = [2, 2, 3, 3, 3, 2, 2];
-    static levelRarityConfig = [3, 3, 3];
+    static levelConfig = [2, 2, 3, 3];
+    static levelRarityConfig = [1, 1, 1, 2, 3];
 
     constructor(public cells: CellConfig[], public delay: Delay, public readonly colors: string[]) {
     }
 
     public static generate(seed: string | number): AnimationConfig {
         const r = Random.fresh(seed);
-        const divider = new AreaDivider(r.randomItems(AnimationConfig.levelConfig, r.randomItems(AnimationConfig.levelRarityConfig, 1)[0]), 16, 16, r);
+        const divider = new AreaDivider(r.randomItems(AnimationConfig.levelConfig, r.randomItems(AnimationConfig.levelRarityConfig, 1)[0]), 8, 8, r);
         const divisions = divider.divide().flatten();
         const cells = divisions.map(d => {
             return new CellConfig(d, AnimationConfig.wowConfig(d, r));
